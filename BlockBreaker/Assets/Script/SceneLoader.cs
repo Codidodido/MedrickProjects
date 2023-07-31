881805ee8524b5e72786cc3d6bb0dd808c43e1b9
Amir Yazdan <begal.cmay@gmail.com>
2023-07-31 11:29:41 +0330

    Create start scene
    Add buttons and scripts

---
+++ BlockBreaker/Assets/Script/SceneLoader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScene()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex + 1);
+    }
+}
