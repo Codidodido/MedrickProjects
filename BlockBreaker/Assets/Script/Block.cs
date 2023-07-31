5af0c392c6dc4c91d49b81e5d62fd32f12994afe
Amir Yazdan <begal.cmay@gmail.com>
2023-07-31 17:42:12 +0330

    Add vfx

--- BlockBreaker/Assets/Script/Block.cs
+++ BlockBreaker/Assets/Script/Block.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip BreakSound;
+    [SerializeField] GameObject blockSparkFLX;
     Level level;
     
     private void Start()
@@ -20,9 +21,26 @@ public class Block : MonoBehaviour
 
     private void DestroyBlock()
     {
+        PlaySoundOnDestroy();
+        level.DecreaseBreakableBlocks();
+        DestroyAct();
+    }
+
+    private void PlaySoundOnDestroy()
+    {
         FindObjectOfType<GameStatus>().AddScore();
         AudioSource.PlayClipAtPoint(BreakSound, Camera.main.transform.position);
-        level.DecreaseBreakableBlocks();
+    }
+
+    private void PlayVFX()
+    {
+        GameObject VFX = Instantiate(blockSparkFLX, transform.position, transform.rotation);
+        Destroy(VFX, 1f);
+    }
+
+    private void DestroyAct()
+    {
+        PlayVFX();
         Destroy(gameObject);
     }
 }
