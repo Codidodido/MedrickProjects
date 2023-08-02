ef00f9ba5c6817eb9c4b8a7fe6f461f0622ccdf9
Amir Yazdan <begal.cmay@gmail.com>
2023-08-02 15:31:01 +0330

    Shot laser

--- LaserDefender/Assets/Script/Player.cs
+++ LaserDefender/Assets/Script/Player.cs
@@ -6,11 +6,14 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] float moveSpeed;
-
+    [SerializeField] GameObject Laser;
+    [SerializeField] Vector2 LaserPos;
+    [SerializeField] float LaserSpeed;
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
     }
 
     private void Move()
@@ -20,5 +23,15 @@ public class Player : MonoBehaviour
         var newXpos = transform.position.x + deltaX;
         var newYpos = transform.position.y + deltaY;
         transform.position = new Vector2(newXpos,newYpos);
+    }
+
+    private void Fire()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject laser = Instantiate(Laser, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0f,LaserSpeed);
+        }
+        
     }
 }
