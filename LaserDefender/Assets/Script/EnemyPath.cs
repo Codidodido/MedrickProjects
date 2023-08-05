4114a432b32621a71ca3587b1b3e9c17183560ab
Amir Yazdan <begal.cmay@gmail.com>
2023-08-05 11:02:44 +0330

    Create enemy movement
    Enemy moves

--- LaserDefender/Assets/Script/EnemyPath.cs
+++ LaserDefender/Assets/Script/EnemyPath.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class EnemyPath : MonoBehaviour
 {
-    [SerializeField] List<Transform> way = new List<Transform>();
+    [SerializeField] List<Transform> way;
+    [SerializeField] float MoveSpeed;
+    int pathIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = way[pathIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveObject();
+    }
+
+    void MoveObject()
+    {
+        if(pathIndex <= way.Count - 1)
+        {
+            var targetPos = way[pathIndex].transform.position;
+            var movement = MoveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, movement);
+
+            if(transform.position == targetPos)
+            {
+                pathIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
